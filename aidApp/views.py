ccb6738f704689455e300c1629517a88157f4171
Ennyola <medunoyeeni@gmail.com>
2021-06-24 15:22:54 +0100

    finished views

--- aidApp/views.py
+++ aidApp/views.py
@@ -2,8 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-<<<<<<< HEAD
     return HttpResponse("<h1>Medical Aid 2 app Homepage</h1>")
-=======
-    return HttpResponse("<h1>Medical Aid app 2 Homepage</h1>")
->>>>>>> d898b2c33a6e4e7454045cd3f5a831524df203b6
