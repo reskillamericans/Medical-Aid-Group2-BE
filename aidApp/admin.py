c5e850b7b4dff2b2fbb41635a9a5e28cdae26982
PeterGodoy <67289778+PeterGodoy@users.noreply.github.com>
2021-07-01 08:13:21 -0700

    registered models

--- aidApp/admin.py
+++ aidApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Doctors
+from .models import Clinics
+from .models import Pharmacies
+from .models import Patients
+from .models import Consultations
+from .models import Feedback_Complaint
+from .models import FAQ
+
 
 # Register your models here.
+admin.site.register(Doctors)
+admin.site.register(Clinics)
+admin.site.register(Pharmacies)
+admin.site.register(Patients)
+admin.site.register(Consultations)
+admin.site.register(Feedback_Complaint)
+admin.site.register(FAQ)
--- aidApp/models.py
+++ aidApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class Doctors(models.Model):
     first_name = models.CharField(max_length=50)
     last_name= models.CharField(max_length=50)
