bfc1b193cf763137eb0cc19d537ef5b71bc0e1b3
Kat Coles <katc.512@gmail.com>
2021-07-01 10:35:14 -0500

    Added models to admin

--- aidApp/admin.py
+++ aidApp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Doctors, Clinics, Pharmacies, Patients, Consultations, Feedback_Complaint, FAQ
+
+admin.site.register(Doctors)
+admin.site.register(Clinics)
+admin.site.register(Pharmacies)
+admin.site.register(Patients)
+admin.site.register(Consultations)
+admin.site.register(Feedback_Complaint)
+admin.site.register(FAQ)
